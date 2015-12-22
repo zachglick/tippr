3f4b60bce8552696ea3e2718eb36d9ca35924824
zachglick <glickzachary@gmail.com>
2015-12-21 18:59:17 -0600

    Initial Commit

---
+++ tipprTests/tipprTests.swift
@@ -0,0 +1,36 @@
+//
+//  tipprTests.swift
+//  tipprTests
+//
+//  Created by Zach Glick on 12/21/15.
+//  Copyright © 2015 Zach Glick. All rights reserved.
+//
+
+import XCTest
+@testable import tippr
+
+class tipprTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
