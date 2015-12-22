9ce3e954546f5cc97b0b857e9de1db80d290273a
zachglick <glickzachary@gmail.com>
2015-12-21 20:58:51 -0600

    Minimum Requirements

--- tippr/ViewController.swift
+++ tippr/ViewController.swift
@@ -10,8 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        super.title = "tippr"
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +31,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        
+        var tipPercentages = [0.18, 0.20, 0.25]
+        var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        
+        var billAmount = NSString(string: billField.text!).doubleValue
+        var tip = billAmount * tipPercentage
+        var total = tip + billAmount
+        
+        
+        tipLabel.text = "$\(tip)"
+        totalLabel.text = "$\(total)"
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        tipLabel.text = String(format: "$%.2f", tip)
+        
+    }
 
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
 }
 
